3f857d370cb7bbf44ef01ecd6bc63613d2c13c23
hackxsaras <62321819+hackxsaras@users.noreply.github.com>
2021-03-18 16:33:43 +0530

    Hierarchy and scrolling on the root div working with z-index and stuff

--- js/main.js
+++ js/main.js
@@ -7,20 +7,28 @@ document.addEventListener('mouseup', CAD_OnUp);
 
 function CAD_OnDown(event){
 
-	start = {x: event.clientX, y: event.clientY};
-	eb = document.elementFromPoint(start.x, start.y);
+	start = {x: event.pageX, y: event.pageY};
+	eb = document.elementFromPoint(start.x - window.pageXOffset, start.y - window.pageYOffset);
 	
+	console.log(eb);
+
+
+	start = relativePosition(eb, start);
+	
+
 	document.addEventListener('mousemove', CAD_OnDrag);
 	
 }
 function CAD_OnDrag(event){
+
+	end = relativePosition(eb, {x: event.pageX, y: event.pageY});
 	
-	end = {x: event.clientX, y: event.clientY};
-	
+
 	if(!cad){
 		cad = document.createElement('div');
 		cad.classList.add('cad-element');
-		document.body.appendChild(cad);
+		eb.appendChild(cad);
+		cad.style.zIndex = (window.getComputedStyle(eb, null).getPropertyValue("zIndex") || 0) + 1;
 	}	
 	let x = Math.min(start.x, end.x);
 	let y = Math.min(start.y, end.y);
@@ -36,4 +44,11 @@ function CAD_OnDrag(event){
 function CAD_OnUp(){
 	document.removeEventListener('mousemove', CAD_OnDrag);
 	cad = null;
+	eb = null;
+}
+
+
+function relativePosition(elem, start){
+	
+	return {x: start.x - elem.getBoundingClientRect().left - window.scrollX, y: start.y - elem.getBoundingClientRect().top - window.scrollY};
 }
